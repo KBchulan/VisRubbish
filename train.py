8eb46bac2a965c882ef6cbc7451e85feca83a3d3
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2024-10-07 15:04:35 +0800

    123

--- train.py
+++ train.py
@@ -1,19 +1,19 @@
 from ultralytics import YOLOv10
 
 # 模型配置路径
-model_yaml_path = r'yolov10s.yaml'
+model_yaml_path = "./yolov10s.yaml"
 
 # 数据集路径
-data_yaml_path = r'data\data.yaml' 
+data_yaml_path = "./data/data.yaml"
 
 # 预训练模型路径
-pre_model_name = r'yolov10s.pt'
+pre_model_name = "./yolov10s.pt"
 
 if __name__ == '__main__':
     model = YOLOv10(model_yaml_path).load(pre_model_name)
 
     results = model.train(data = data_yaml_path,
-                          epochs = 20,
+                          epochs = 10,
                           batch = 2,
                           name = 'trash'
                           )
